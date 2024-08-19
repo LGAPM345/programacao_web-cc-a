201b1d5a3e652a227dc8cd008aa3d44427998617
LGAPM345 <luis.pmelo@sempreceub.com>
2024-08-19 10:13:35 -0300

    Alterações no java script

--- script.js
+++ script.js
@@ -7,4 +7,15 @@ function inserir() {
     console.log(ElementoPlaneta);
     let ElementoInstrumento = document.getElementById("instrumento").value;
     console.log(ElementoInstrumento);
+
+    console.log(validaelemento(ElementoPlaneta));
+    console.log(validaelemento(ElementoInstrumento));
 }
+
+    function  validaelemento(ElementoPlaneta){
+        return /^[a-zA-Z\s+]+$/.test(ElementoPlaneta);
+        
+}
+    function validaelemento(ElementoInstrumento){
+        return /^[a-zA-Z\s+]+$/.test(ElementoInstrumento);
+    }
